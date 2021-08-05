ef5aa880d547ef35e96e798a1aa9854952888643
ahmadfarhanstwn <ahmadfarhanstwn@gmail.com>
2021-08-05 11:28:21 +0700

    first commit

---
+++ ReverseInteger.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        output = []
+        for i in str(x):
+            if i != '-':
+                output.insert(0, i)
+        
+        if int(''.join(output)) >= 2**31-1 or int(''.join(output)) <= -2**31:
+            return 0
+        if x < 0:
+            return -int(''.join(output))
+        else:
+            return int(''.join(output))
+
+//28 ms runtime
